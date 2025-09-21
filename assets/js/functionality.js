58282f59830d48edfb247ec244443dc24ae741b0
Khushibhalala <khushibhalala.monarch@gmail.com>
2025-09-21 17:52:27 +0530

    Initial website upload

---
+++ assets/js/functionality.js
@@ -0,0 +1,13 @@
+//custom alert
+function customAlert() {
+  document.getElementById("custom-alert").style.display = "block";
+}
+
+function closeAlert() {
+  document.getElementById("custom-alert").style.display = "none";
+}
+
+// login from
+function LogIn() {
+  window.location.href = "index.html";
+}
